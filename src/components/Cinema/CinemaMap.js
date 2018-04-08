6f4aaeff7897a7d1cb0382e05639f83e53f1ade5
Zero Two <vyphamhung10@gmail.com>
2018-04-08 14:08:32 +0700

    Change google map api key

--- src/components/Cinema/CinemaMap.js
+++ src/components/Cinema/CinemaMap.js
@@ -5,7 +5,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-map
 const CinemaMap = compose(
   withProps({
     googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDgZCMsOED9D92Lx5kpYNBP7VVEUXAsk2I&v=3.exp&libraries=geometry,drawing,places',
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyBvPAOAi-dx00iYy0j1Xk9lE8WyixmHaJA&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{height: '100%'}} />,
     containerElement: <div style={{height: '300px', position: 'relative', marginTop: 10, overflow: 'hidden'}} />,
     mapElement: <div style={{height: '100%'}} />
